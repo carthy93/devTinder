ceaeb60f3d8a02572538494de2b885672032a038
Karthik Balachandar <caarthik180@gmail.com>
2026-08-17 03:19:17 +0530

    Remove tracked .DS_Store and ignore it; log query params on /user

--- src/app.js
+++ src/app.js
@@ -1,37 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "John", lastName: "Doe" });
 });
-
-app.post("/user", (req, res) => {
-  //save data to db
-  res.send("Data has been saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  //delete data from db
-  res.send("Data has been deleted from the database");
-});
-
-app.put("/user", (req, res) => {
-  //update data in db
-  res.send("Data has been updated in the database");
-});
-
-app.patch("/user", (req, res) => {
-  //update data in db
-  res.send("Data has been updated in the database");
-});
-
-// app.use("/test", (req, res) => {
-//   res.send("Hello from the Test route!");
-// });
-
-// app.use("/", (req, res) => {
-//   res.send("Hello from the dashboard!");
-// });
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
