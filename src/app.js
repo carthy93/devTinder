c6947ed567759cf059ee005b5b19801263229a45
Karthik Balachandar <caarthik180@gmail.com>
2026-08-19 11:14:46 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-// handle auth for all get, delete, and update requests
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res, next) => {
-  // Logic of getting user data
-  res.send("User data fetched successfully");
+app.get("/getUserData", (req, res, next) => {
+  // Logic of DB call and fetching user data
+  // what if there is some error in DB call
+  try {
+    throw new Error("Database connection failed");
+    res.send("User data fetched successfully");
+  } catch (error) {
+    {
+      res.status(500).send("Something wrong. Contact admin");
+    }
+    throw new Error("Database connection failed");
+    res.send("User data fetched successfully");
+  }
 });
 
-app.get("/admin/getAlldata", (req, res, next) => {
-  // Logic of checking if the request is authenticated or not
-  res.send("All data fetched successfully");
-});
-
-app.get("/admin/deleteUser", (req, res, next) => {
-  // Logic of deleting a user
-  res.send("User deleted successfully");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(7777, () => {
