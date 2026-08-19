bcbecd8666f575bf564af1d009a9bb23bf54f3b9
Karthik Balachandar <caarthik180@gmail.com>
2026-08-20 02:41:33 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Sachin",
+    lastName: "Tendulkar",
+    emailId: "sachin@gmail.com",
+    password: "xyz",
+  });
+
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
