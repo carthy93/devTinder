78808258a16306874a6dfe247eb1cbbe56edfd69
Karthik Balachandar <caarthik180@gmail.com>
2026-08-19 11:17:12 +0530

    wildcard-error- handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/getUserData", (req, res, next) => {
+app.get("/getUserData", (req, res) => {
   // Logic of DB call and fetching user data
   // what if there is some error in DB call
-  try {
-    throw new Error("Database connection failed");
-    res.send("User data fetched successfully");
-  } catch (error) {
-    {
-      res.status(500).send("Something wrong. Contact admin");
-    }
-    throw new Error("Database connection failed");
-    res.send("User data fetched successfully");
-  }
+  // try {
+  throw new Error("Database connection failed");
+  res.send("User data fetched successfully");
+  // } catch (error) {
+  //   {
+  //     res.status(500).send("Something wrong. Contact admin");
+  //   }
+  // }
 });
 
 app.use("/", (err, req, res, next) => {
